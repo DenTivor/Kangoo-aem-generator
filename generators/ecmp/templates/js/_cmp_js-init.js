c254f8bcdd1de7ee0b4c64bbf53d6f3082d971f1
Roman Alekseev <roman.alekseev.ru@gmail.com>
2017-02-07 18:30:27 +0300

    CodeReview comments

--- generators/ecmp/templates/js/_cmp_js-init.js
+++ generators/ecmp/templates/js/_cmp_js-init.js
@@ -3,13 +3,11 @@ define(["jquery","underscore", "components/<%= name%>/<%= name%>-controller"], f
     var settings = {
        elSelector: ".<%= name%>[element-id='<%= name%>']",
 
-    };
-     
-    var elSettings = settings;
-
+    },
+    elSettings = settings,
     els = $(settings.elSelector);
 
-    _.each(els, function(el, key) {
+    _.each(els, function(el) {
         elSettings.el = $(el);
         new <%= jsVarsName%>Controller(settings);
     })
